bccf160b7e5137bfa89d0f5b1ec0eb1924251680
John Lombard <john.lombard1996@gmail.com>
2020-01-07 14:52:08 -0500

    formatted not found

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div className="column is-10 is-offset-1">
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
